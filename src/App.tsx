e3273e612a8bb153439061c289e4bb9c092b89b9
Erika Tsuchiya <tsuchiya.erika@synergy101.jp>
2019-11-04 22:07:32 +0900

    ちょっと修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React, {FC} from 'react';
-import logo from './logo.svg';
+import React, {FC, useState, useEffect} from 'react';
+import {List, Item, Header} from 'semantic-ui-react';
 import './App.css';
 
 const App: FC = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    // TODO BEからリストを取得する処理
+    return;
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header>TODOリスト</Header>
+      <Item>
+        <List.Item as="li">
+          なし
+        </List.Item>
+      </Item>
     </div>
   );
 }
