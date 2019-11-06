3dad271aead278749a370f0a8327d23f1104ead8
Erika Tsuchiya <tsuchiya.erika@synergy101.jp>
2019-11-06 23:55:10 +0900

    add tmp data

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,41 @@ import {List, Item, Header} from 'semantic-ui-react';
 import './App.css';
 
 const App: FC = () => {
-  const [list, setList] = useState([]);
+  // var obj:{id:number, task:string};
+  const [list, setList] = useState();
 
-  useEffect(() => {
-    // TODO BEからリストを取得する処理
-    return;
-  }, [])
+  const getTodoList = () => {
+    const tmp = {
+      id: 1,
+      task: "サンプル",
+    };
+    const tmp2 = {
+      id: 2,
+      task: "サンプル2",
+    };
+    // var array:{id:number, task:string}[] = [tmp, tmp2];
+    return tmp2.id;
+  }
+  // useEffect(() => {
+  //   // TODO BEからリストを取得する処理
+  //   const tmp = {
+  //     id: 1,
+  //     task: "サンプル",
+  //   };
+  //   const tmp2 = {
+  //     id: 2,
+  //     task: "サンプル2",
+  //   };
+  //   var array:{id:number, task:string}[] = [tmp, tmp2];
+  //   setList(array)
+  // }, [])
 
   return (
     <div className="container">
       <Header>TODOリスト</Header>
       <Item>
         <List.Item as="li">
-          なし
+          {getTodoList()}
         </List.Item>
       </Item>
     </div>
