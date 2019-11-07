e77b469a9a074b629c941a2342447fc904eeca01
Erika Tsuchiya <tsuchiya.erika@synergy101.jp>
2019-11-08 00:29:22 +0900

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,42 @@
-import React, {FC, useState, useEffect} from 'react';
-import {List, Item, Header} from 'semantic-ui-react';
+import React, {FC} from 'react';
+import {Item, Header} from 'semantic-ui-react';
 import './App.css';
 
-const App: FC = () => {
-  // var obj:{id:number, task:string};
-  const [list, setList] = useState();
+interface Todo {
+  id: number;
+  name: string;
+  remarks?: string;
+}
 
-  const getTodoList = () => {
-    const tmp1 = {
-      id: 1,
-      task: "サンプル",
-      done: false
-    };
-    const tmp2 = {
-      id: 2,
-      task: "サンプル2",
-      done: false,
-    };
-    var array:{id:number, task:string, done:boolean}[] = [];
-    array.push(tmp1, tmp2)
-    return array;
-  }
-  // useEffect(() => {
-  //   // TODO BEからリストを取得する処理
-  //   const tmp = {
-  //     id: 1,
-  //     task: "サンプル",
-  //   };
-  //   const tmp2 = {
-  //     id: 2,
-  //     task: "サンプル2",
-  //   };
-  //   var array:{id:number, task:string}[] = [tmp, tmp2];
-  //   setList(array)
-  // }, [])
+
+interface TodoList { 
+  tasks: Todo[]; 
+}
+
+const App: FC = () => {
+  const tmp: TodoList = {
+    tasks: [
+      {
+        id: 1,
+        name: "coding",
+        remarks: "react hooks",
+      }
+    ]
+  };
+
+  const tmp2: Todo = {
+    id: 1,
+    name: "coding",
+    remarks: "react hooks",
+  };
+
 
   return (
     <div className="container">
       <Header>TODOリスト</Header>
       <Item>
-        {getTodoList()}
+
+        {tmp2.id}
           
       </Item>
     </div>
